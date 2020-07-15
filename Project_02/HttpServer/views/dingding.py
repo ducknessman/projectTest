6f98492efce0f40d4a38ac0125f2b3e6345cd70c
ducknessman <1160154212@qq.com>
2020-07-15 14:44:43 +0800

    del some code

--- Project_02/HttpServer/views/dingding.py
+++ Project_02/HttpServer/views/dingding.py
@@ -12,13 +12,6 @@ def DDMsg(msg):
     :ps 下面注释的部分为钉钉告警需要@的人的信息，只需要添加电话号码就可以
     :return:
     '''
-    url = 'https://oapi.dingtalk.com/robot/send?access_token=511b02522a1f0d28e61e68cd874c3b7a5c20cd83efadba558039ab4567a9846e'
-    data = {"msgtype": "text","text": {"content": "自动化测试用例遍历结果：{}".format(msg)},
-            # "at": {
-            #     "atMobiles": [
-            #         "13550629276"
-            #     ],
-            #     "isAtAll": False
-            # }
-            }
+    url = 'https://oapi.dingtalk.com/robot/send'
+    data = {"msgtype": "text","text": {"content": "自动化测试用例遍历结果：{}".format(msg)}}
     response = requests.post(url,json=data)
